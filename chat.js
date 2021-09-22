4d42c1a67533fa401a81ec351f0e1d6b76160df3
saulgutierrez <62368834+saulgutierrez@users.noreply.github.com>
2021-09-22 02:17:45 -0500

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,5 +1,6 @@
 // Make connection
 var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://aqueous-mountain-16622.herokuapp.com/');
 
 // Query DOM
 var message = document.getElementById('message');
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var socket = require('socket.io');
 
 // App setup
 var app = express();
-var server = app.listen(4000, function() {
+var server = app.listen(process.env.PORT || 4000, function() {
     console.log('listening to request on port 4000');
 });
 
